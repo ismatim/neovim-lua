0797211367c6800025c71828ae1f14b3b67f009f
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2023-06-29 10:01:09 -0300

    fix: changed prettier by rome

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -6,15 +6,24 @@ local prettier = function()
     stdin = true
  }
 end
+
+local rome = function()
+  return {
+    exe = "rome ",
+    args = {"format --stdin-file-path", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), ''},
+    stdin = true
+ }
+end
+
 --plugin-search-dir=./dir-with-plugins --plugin=prettier-plugin-foo
 
 require('formatter').setup({
   filetype = {
-    json = { prettier },
-    javascript = { prettier },
-    javascriptreact = { prettier },
-    html= { prettier },
-    css = { prettier },
-    scss = { prettier }
+    json = { rome },
+    javascript = { rome },
+    javascriptreact = { rome},
+    html= { rome },
+    css = { rome },
+    scss = { rome }
   }
 })
