9b3d4ffbab569ed44f329ee5413106bf91a7d7b6
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2024-05-01 09:51:17 -0300

    fix copilot accept

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,7 @@
 
 local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
+local g = vim.g         				-- global variables
 
 -----------------------------------------------------------
 -- Neovim shortcuts:
@@ -93,6 +94,7 @@ map('v', '<leader>td', '<ESC>:!trans -no-ansi de:en ', default_opts)
 map('n', '<leader>td', ':!trans -no-ansi de:en ', default_opts)
 
 -- Copilot
+g.copilot_no_tab_map=true             -- change leader to a comma
 map('n', '<leader>cp', ':Copilot panel<CR>', default_opts)  -- Copilot panel for suggestions
-
+map('i', '<C-j>', 'copilot#Accept("\\<CR>")', {expr = true, noremap = true, silent = true})
 
